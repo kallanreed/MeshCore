7ecf87ea0ab8f68027cfafb9a8c0d32d60543df0
kallanreed <3761006+kallanreed@users.noreply.github.com>
2026-03-14 12:29:36 -0700

    Revert "Show DM ack on nrf-txt UI"
    This reverts commit 3c0ab9612e3cb8d8447ef509089a8a1696d78e37.

--- examples/companion_radio/nrf-txt/ui_task.h
+++ examples/companion_radio/nrf-txt/ui_task.h
@@ -33,13 +33,10 @@ class UITask : public AbstractUITask, public UIViewModel {
   uint32_t _next_render = 0;
   uint32_t _next_batt_check = 0;
   uint32_t _auto_off = 0;
-  uint32_t _dm_ack_expires_at = 0;
-  uint32_t _dm_ack_trip_time_ms = 0;
   //uint32_t _alert_expiry = 0;
   //uint32_t _next_backlight_btn_check = 0;
 
   float _batt_percent = 0;
-  char _dm_ack_label[24] = {};
   MessageBuffer _message_buffer;
   bool _invert_screen = false;
   Bme680HistoryStore _bme680_history;
@@ -60,7 +57,6 @@ class UITask : public AbstractUITask, public UIViewModel {
   void checkAutoOff();
   void updateBme680History();
   Bme680Data readBme680Data();
-  void renderDmAckBadge();
 
 public:
   UITask(mesh::MainBoard* board, BaseSerialInterface* serial)
@@ -81,7 +77,6 @@ public:
     const char* text,
     int msgcount,
     const UIMessageMeta& meta) override;
-  void onDirectMessageAck(const ContactInfo& contact, uint32_t trip_time_ms) override;
   void notify(UIEventType t = UIEventType::none) override;
   void loop() override;
 
